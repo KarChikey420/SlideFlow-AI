ce2e82926d30464ade4a9150e71de92fbd519058
KarChikey <kartikeynegi2002@gmail.com>
2025-12-04 11:11:44 +0530

    add

---
+++ app/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI,Depends,HTTPException
+from fastapi.responses import FileResponse
+from passlib.context import CryptContext
+import uuid 
+from backend.database import SessionLocal,User
+from backend.auth import create_access_token,current_user
+from ppt_content.generator import create_presentation
+from ppt_content.pptx_file import create_ppt
+
+app=FastAPI()
+
+pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+@app.post("/signup")
+def signup(name:str,email:str,password:str):
+    db=SessionLocal()
+    if db.query(User).filter(User.email==email).first():
+        raise HTTPException(400,"User already exists")
+    
+    hashed=pwd_context.hash(password)
+    user=User(name=name, email=email, password=hashed)
+    db.add(user)
+    db.commit()
+    return {"message":"User created successfully"}
+
+@app.post("/login")
+def login(email:str,password:str):
+    db=SessionLocal()
+    
+    user=db.query(User).filter(User.email==email).first()
+    if not user:
+        raise HTTPException(404, "User not found")
+    if not pwd_context.verify(password,user.password):
+        raise HTTPException(401,"Wrong password")
+    token=create_access_token({"sub":user.email})
+    return {"access_token":token, "token_type":"bearer"}
+
+@app.post("/generate_ppx")
+def generate_pptx(topic:str,slide:10,current_user:str= Depends(current_user)):
+    slides=create_presentation(topic,slide)
+    filename=f"{topic.replace('','_')}_{uuid.uuid4().hex}.pptx"
+    create_ppt(slides,filename)
+    
+    return FileResponse(filename, media_type="application/vnd.openxmlformats-officedocument.presentationml.presentation", filename=filename)
+        
+        
