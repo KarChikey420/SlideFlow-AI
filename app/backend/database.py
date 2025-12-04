1df1dbc8cb952a5047292879be90edc0ffeccc0d
KarChikey <kartikeynegi2002@gmail.com>
2025-12-04 09:59:16 +0530

    add

---
+++ app/backend/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import sessionmaker,declarative_base
+
+DATABASE_URL="sqlite:///./test.db"
+engine=create_engine(DATABASE_URL,connect_args={"check_same_thred":False})
+SessionLocal=sessionmaker(bind=engine)
+Base=declarative_base()
+
+class User(Base):
+    __tablename__="users",
+    id=Column(Integer,primary_key=True,index=True)
+    name=Column(String(50))
+    email=Column(String(50))
+    password=Column(String(50))
+    
+Base.metadata.create_all(bind=engine)
+
+    
