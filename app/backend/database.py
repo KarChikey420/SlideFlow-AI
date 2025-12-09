734dbb3b29aab50822c1bf8b68fd2f314ae3fe2b
KarChikey <kartikeynegi2002@gmail.com>
2025-12-09 14:35:40 +0530

    add

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.orm import sessionmaker,declarative_base
+import os
 
-DATABASE_URL="sqlite:///./test.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "test.db")
+
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 engine=create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
 SessionLocal=sessionmaker(bind=engine)
 Base=declarative_base()
